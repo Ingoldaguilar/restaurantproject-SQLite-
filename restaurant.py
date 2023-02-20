f0c8410fc3339d7804a9f0a0a1d483b52598ba37
ingold <ingoldaguilar45@gmail.com>
2023-02-20 11:52:49 -0600

    creating the 'create databases function'

--- restaurant.py
+++ restaurant.py
@@ -1,16 +1,32 @@
 # imports
 import sqlite3
 
-# create connection
-connection = sqlite3.connect(".db")
-# create cursor
-cursor = connection.cursor()
+# functions
 
+# create db
+def create_db():
+    # create connection
+    connection = sqlite3.connect("restaurant.db")
+    # create cursor
+    cursor = connection.cursor()
 
+    cursor.execute("""
+    CREATE TABLE category(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            name VARCHAR(100) UNIQUE NOT NULL
+    ) 
+    """)
 
+    cursor.execute("""
+    CREATE TABLE dish(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            name VARCHAR(100) UNIQUE NOT NULL, 
+            category_id INTEGER NOT NULL, 
+            FOREIGN KEY(category_id) REFERENCES category(id)
+    ) 
+    """)
 
+    # close the connection
+    connection.close()
 
-# commit changes
-connection.commit()
-# close
-connection.close()
+create_db()
